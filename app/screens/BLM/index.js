20fd86830b29b8ff357ab738662096abcfcab2de
Matt Zabriskie <mzabriskie@gmail.com>
2020-06-11 11:42:32 -0600

    Fixing style issues

--- app/screens/BLM/index.js
+++ app/screens/BLM/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default () => {
+  useEffect(() => {
+    const container = document.getElementById('container');
+
+    container.classList.add('fullscreen');
+
+    return () => container.classList.remove('fullscreen');
+  }, [])
+
   return (
     <div className="BlackLivesMatter">
       <section>
